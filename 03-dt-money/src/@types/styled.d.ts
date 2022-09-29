6022b88440b71e8cb998dd834e77bbde3747c7e4
marcoscode404 <viniciusmg99@outlook.com>
2022-09-29 08:10:06 -0400

    configuração de styles do projeto

---
+++ 03-dt-money/src/@types/styled.d.ts
@@ -0,0 +1,9 @@
+// extende/adiciona a tipagem para o typescript
+import 'styled-components'
+import { defaultTheme } from "../styles/themes/default";
+
+type ThemeType = typeof defaultTheme
+
+declare module 'styled-components' {
+    export interface DefaultTheme extends ThemeType {}
+}
