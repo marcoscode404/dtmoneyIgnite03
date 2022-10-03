b4ac417870d17475b89aaca2d11b847b97c13dc7
marcoscode404 <viniciusmg99@outlook.com>
2022-10-03 16:46:27 -0400

    criando e estilizando botao de entrada e saida

--- 03-dt-money/src/components/NewTransactionModal/styles.ts
+++ 03-dt-money/src/components/NewTransactionModal/styles.ts
@@ -60,7 +60,7 @@ export const Content = styled(Dialog.Content)`
   }
 `
 
-// puxando o Dialog do radix - estilizando o Titulo
+// puxando o Dialog do radix - estilizando o Close de fechar modal
 export const CloseButton = styled(Dialog.Close)`
   position: absolute;
   background: transparent;
@@ -70,4 +70,38 @@ export const CloseButton = styled(Dialog.Close)`
   line-height: 0;
   cursor: pointer;
   color: ${(props) => props.theme['gray-500']};
+`
+
+// TransactionType
+export const TransactionType = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  margin-top: 0.5rem;
+`
+
+// interface TransactionTypeButtonProps
+interface TransactionTypeButtonProps {
+  variant: 'income' | 'outcome'
+}
+
+// TransactionTypeButton
+export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+  background: ${(props) => props.theme['gray-700']};
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  border-radius: 6px;
+  cursor: pointer;
+  border: 0;
+  color: ${(props) => props.theme['gray-300']};
+
+  svg {
+    color: ${(props) =>
+      props.variant === 'income'
+        ? props.theme['green-300']
+        : props.theme['red-300']};
+  }
 `
