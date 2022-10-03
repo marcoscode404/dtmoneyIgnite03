52a279cec2b110e87b905a0a84b1451513364692
marcoscode404 <viniciusmg99@outlook.com>
2022-10-03 16:28:17 -0400

    Criando estilização da modal / Acessibilidade no Projeto /

--- 03-dt-money/src/components/Header/index.tsx
+++ 03-dt-money/src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 // logo
 import logoImg from '../../assets/logo.svg'
 import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
   return (
@@ -17,17 +18,7 @@ export function Header() {
             <NewTransactionButton>Nova Transação</NewTransactionButton>
           </Dialog.Trigger>
 
-          {/* ajuda a colocar o conteudo do modal fora de todas as divs */}
-          <Dialog.Portal>
-            <Dialog.Overlay>
-              <Dialog.Content>
-                <Dialog.Title>Nova Transação</Dialog.Title>
-
-                {/* botao de fechar */}
-                <Dialog.Close />
-              </Dialog.Content>
-            </Dialog.Overlay>
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
