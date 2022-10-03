e528238435ad1e0b6ba2e02a6fc80111b8f2721a
marcoscode404 <viniciusmg99@outlook.com>
2022-10-03 14:59:59 -0400

    cores nos valores positivos e negativos

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,6 +1,10 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
-import { TransactionsContainer } from './styles'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
@@ -10,18 +14,22 @@ export function Transactions() {
 
       {/* table */}
       <TransactionsContainer>
-        <table>
+        <TransactionsTable>
           <tbody>
             <tr>
               <td width="50%">Desenvolvimento de site</td>
-              <td>R$ 12.000,00</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
               <td>Venda</td>
               <td>13/04/2022</td>
             </tr>
 
             <tr>
               <td width="50%">Hot-Dog</td>
-              <td>-R$ 59,00</td>
+              <td>
+                <PriceHighlight variant="outcome">-R$ 59,00</PriceHighlight>
+              </td>
               <td>Alimentação</td>
               <td>13/05/2022</td>
             </tr>
@@ -33,7 +41,7 @@ export function Transactions() {
               <td>13/04/2022</td>
             </tr>
           </tbody>
-        </table>
+        </TransactionsTable>
       </TransactionsContainer>
     </div>
   )
