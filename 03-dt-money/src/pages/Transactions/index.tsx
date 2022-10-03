8c0fa06e11e4b289b05a42cc02d95e8dfafb54e4
marcoscode404 <viniciusmg99@outlook.com>
2022-10-03 14:35:37 -0400

    alinhando a tabela

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { TransactionsContainer } from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      {/* table */}
+      <TransactionsContainer>
+        <table>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>R$ 12.000,00</td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hot-Dog</td>
+              <td>-R$ 59,00</td>
+              <td>Alimentação</td>
+              <td>13/05/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>R$ 12.000,00</td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </table>
+      </TransactionsContainer>
     </div>
   )
 }
